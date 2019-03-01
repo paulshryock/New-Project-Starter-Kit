b0918855d2d1fd9cb9ce784fc964712ce4b514ae
Paul Shryock <paul@pshry.com>
2019-03-01 10:36:01 -0500

    Install @babel/polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',
 
   entry: {
+    'polyfill': '@babel/polyfill',
     'app': './src/_assets/js/app.js'
   },
 
