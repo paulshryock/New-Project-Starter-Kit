ff2895e88b62fc1ddadda0274653098862b52487
Paul Shryock <paul@pshry.com>
2019-09-10 14:05:55 -0400

    Clean js

--- _scripts/build.js
+++ _scripts/build.js
@@ -1,7 +1,7 @@
 const Eleventy = require('@11ty/eleventy')
 
-(async function() {
-  let inst = new Eleventy()
+(async function () {
+  const inst = new Eleventy()
   await inst.init()
   await inst.write()
 })()
