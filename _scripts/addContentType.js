a6042fe53efebd8516c2c36ff2076940d7b3b90c
Paul Shryock <paul@pshry.com>
2019-10-02 20:18:20 -0400

    Simplify add content script

--- _scripts/addContentType.js
+++ _scripts/addContentType.js
@@ -22,26 +22,24 @@ addContent.init = () => {
   * Get parameters
   */
 addContent.getParameters = () => {
-  rl.question('Create content type for which platform? ', (platform) => {
-    rl.question(`What kind of ${platform} content? `, (type) => {
-      rl.question(`What is the plural form of ${type}? `, (typePlural) => {
-        rl.question(`Should ${platform} ${typePlural} live in a subdirectory? (true/false) `, (hasSubdirectory) => {
-          rl.question(`Do you want to create a new ${type} layout? (true/false) `, (addLayout) => {
-            // TODO: Restrict hasSubdirectory input to a true boolean instead of a string,
-            //        or restrict input to 'true' or 'false' strings
-            let hasSubdirectoryBool = false
-            if (hasSubdirectory === 'true') {
-              hasSubdirectoryBool = true
-            }
-            // TODO: Restrict addLayout input to a true boolean instead of a string,
-            //        or restrict input to 'true' or 'false' strings
-            let addLayoutBool = false
-            if (addLayout === 'true') {
-              addLayoutBool = true
-            }
-            addContent.folder(platform, type, typePlural, hasSubdirectoryBool, addLayoutBool)
-            rl.close()
-          })
+  rl.question(`What kind of content would you like to create? (singular, lowercase) `, (type) => {
+    rl.question(`What is the plural form of ${type}? `, (typePlural) => {
+      rl.question(`Should ${typePlural} live in a subdirectory? (true/false) `, (hasSubdirectory) => {
+        rl.question(`Do you want to create a new ${type} layout? (true/false) `, (addLayout) => {
+          // TODO: Restrict hasSubdirectory input to a true boolean instead of a string,
+          //        or restrict input to 'true' or 'false' strings
+          let hasSubdirectoryBool = false
+          if (hasSubdirectory === 'true') {
+            hasSubdirectoryBool = true
+          }
+          // TODO: Restrict addLayout input to a true boolean instead of a string,
+          //        or restrict input to 'true' or 'false' strings
+          let addLayoutBool = false
+          if (addLayout === 'true') {
+            addLayoutBool = true
+          }
+          addContent.folder(type, typePlural, hasSubdirectoryBool, addLayoutBool)
+          rl.close()
         })
       })
     })
@@ -49,51 +47,49 @@ addContent.getParameters = () => {
 }
 
 /**
-  * Add content folder: (/src/${platform}/${typePlural})
+  * Add content folder: (/src/${typePlural})
   *
-  * param {string} platform
   * param {string} type
   * param {string} typePlural
   * param {boolean} hasSubdirectory
   * param {boolean} addLayout
   */
-addContent.folder = (platform, type, typePlural, hasSubdirectory, addLayout) => {
-  const path = `./src/${platform}/${typePlural}`
+addContent.folder = (type, typePlural, hasSubdirectory, addLayout) => {
+  const path = `./src/${typePlural}`
   const mask = 484
 
   fs.mkdir(path, mask, function (err) {
     if (err) {
       if (err.code === 'EEXIST') {
-        console.error(`${platform} ${typePlural} already exists: ${path}`)
+        console.error(`${typePlural} already exists: ${path}`)
       } else {
         console.error(err)
       }
       return false
     } else {
       console.log(`Adding content folder: ${path}`)
-      addContent.data(path, platform, type, hasSubdirectory, typePlural, addLayout)
+      addContent.data(path, type, hasSubdirectory, typePlural, addLayout)
     }
   })
 }
 
 /**
-  * Add content directory data file: (/src/${platform}/${typePlural}/${typePlural}.11tydata.js)
+  * Add content directory data file: (/src/${typePlural}/${typePlural}.11tydata.js)
   *
   * param {string} path
-  * param {string} platform
   * param {string} type
   * param {boolean} hasSubdirectory
   * param {string} typePlural
   * param {boolean} addLayout
   */
-addContent.data = (path, platform, type, hasSubdirectory, typePlural, addLayout) => {
+addContent.data = (path, type, hasSubdirectory, typePlural, addLayout) => {
   const file = `${path}/${typePlural}.11tydata.js`
   const subdirectory = hasSubdirectory === true ? `${typePlural}/` : ''
   const data = `module.exports = function () {
   return {
     contentType: '${type}',
-    layout: '${platform}/${type}',
-    permalink: './${platform}/${subdirectory}{{ slug }}/index.html'
+    layout: '${type}',
+    permalink: './${subdirectory}{{ slug }}/index.html'
   }
 }
 `
@@ -104,24 +100,23 @@ addContent.data = (path, platform, type, hasSubdirectory, typePlural, addLayout)
       console.error(err)
       return false
     } else {
-      console.log(`Added ${platform} ${typePlural} data file: ${file}`)
-      addContent.layout(platform, type, typePlural, addLayout)
+      console.log(`Added ${typePlural} data file: ${file}`)
+      addContent.layout(type, typePlural, addLayout)
     }
   })
 }
 
 /**
-  * Add layout file: (/src/_layouts/${platform}/${type}.liquid)
+  * Add layout file: (/src/_layouts/${type}.liquid)
   *
-  * param {string} platform
   * param {string} type
   * param {string} typePlural
   * param {boolean} addLayout
   */
-addContent.layout = (platform, type, typePlural, addLayout) => {
-  const file = `./src/_layouts/${platform}/${type}.liquid`
+addContent.layout = (type, typePlural, addLayout) => {
+  const file = `./src/_layouts/${type}.liquid`
   const data = `---
-layout: ${platform}/global
+layout: global
 ---`
   const options = { flag: 'ax' }
 
@@ -131,7 +126,7 @@ layout: ${platform}/global
         console.error(err)
         return false
       } else {
-        console.log(`Added ${platform} ${type} layout file: ${file}`)
+        console.log(`Added ${type} layout file: ${file}`)
       }
     })
   }
