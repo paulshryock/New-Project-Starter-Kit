c0b907e7240d606548f0159de5dd560d60b613e2
Paul Shryock <paul@pshry.com>
2020-05-26 11:36:28 -0400

    Many updates

--- modules/lighthouse.js
+++ modules/lighthouse.js
@@ -1,31 +1,46 @@
-const config = require('config')
-const lighthouse = require('lighthouse')
 const chromeLauncher = require('chrome-launcher')
+const lighthouse = require('lighthouse')
+const config = require('config')
+const url = config.get('site.url')
 
-function launchChromeAndRunLighthouse (url, opts, lighthouseConfig = null) {
-  return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then(chrome => {
-    opts.port = chrome.port
-    return lighthouse(url, opts, lighthouseConfig).then(results => {
-      // use results.lhr for the JS-consumeable output
-      // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
-      // use results.report for the HTML/JSON/CSV output as a string
-      // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-      return chrome.kill().then(() => results.lhr)
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags})
+    .then(chrome => {
+      opts.port = chrome.port
+
+      return lighthouse(url, opts, config)
+        .then(results => {
+          // use results.lhr for the JS-consumable output
+          // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+          // use results.report for the HTML/JSON/CSV output as a string
+          // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+          return chrome.kill()
+            .then(() => results.lhr)
+        })
     })
-  })
 }
-
-const url = `http://localhost:${config.get('server.port')}`
 
 const opts = {
   chromeFlags: ['--show-paint-rects']
 }
 
-// Configure Lighthouse:
-const lighthouseConfig = {}
+const configuration = {
+  extends: 'lighthouse:default',
+  settings: {
+    onlyCategories: ['performance'],
+    // onlyAudits: [
+    //   'first-meaningful-paint',
+    //   'speed-index',
+    //   'first-cpu-idle',
+    //   'interactive',
+    // ],
+  },
+}
 
-// Usage:
-launchChromeAndRunLighthouse(url, opts, lighthouseConfig).then(results => {
-  // Use results!
-  console.log(results)
-})
+launchChromeAndRunLighthouse(url, opts, configuration)
+  .then(results => {
+    Object.keys(results.audits).forEach(key => {
+      const result = results.audits[key]
+      console.log(`${result.title}: ${result.displayValue}`)
+    })
+  })
